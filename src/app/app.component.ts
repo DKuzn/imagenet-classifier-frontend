a800ec6eadbb2d86ebffcf59122fcd1801d12773
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2023-12-18 13:56:10 +0300

    Update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    ToolbarComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
