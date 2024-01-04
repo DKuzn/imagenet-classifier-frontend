22112ba37243348727cb2db01395a3ef614db5db
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-01-04 22:05:09 +0300

    Add routing

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule,
+    RouterOutlet,
     MatToolbarModule,
     MatButtonModule
   ],
