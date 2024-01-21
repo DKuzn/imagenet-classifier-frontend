b70a2cb67562927969934758843492fcc6895756
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-01-21 23:32:00 +0300

    Add new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,31 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'profile', component: ProfileComponent},
-    { path: '',   redirectTo: 'home', pathMatch: 'full' }
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        children: [
+            {
+                path: 'auth',
+                component: AuthorizationComponent
+            },
+            {
+                path: 'signup',
+                component: SignupComponent
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
 ];
