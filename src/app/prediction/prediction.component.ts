78199d5634128489f1c28309b0749a24f51c7147
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2024-01-24 23:45:47 +0300

    Update Prediction component

--- src/app/prediction/prediction.component.ts
+++ src/app/prediction/prediction.component.ts
@@ -1,10 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-prediction',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './prediction.component.html',
   styleUrl: './prediction.component.css'
 })
@@ -13,4 +19,11 @@ export class PredictionComponent {
   @Input() datetime!: string;
   @Input() prediction!: string;
   @Input() location!: any;
+
+  public formatLocation() {
+    let latitude = String(this.location.latitude);
+    let longitude = String(this.location.longitude);
+
+    return latitude + " " + longitude;
+  }
 }
